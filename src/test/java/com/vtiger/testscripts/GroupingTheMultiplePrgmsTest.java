b873cdc6dccc1c4ed9841727fe80c481d2bbf0ba
saykamstephen@gmail.com <saykamstephen@gmail.com>
2021-11-16 17:47:40 +0530

    edited

--- src/test/java/com/vtiger/testscripts/GroupingTheMultiplePrgmsTest.java
+++ src/test/java/com/vtiger/testscripts/GroupingTheMultiplePrgmsTest.java
@@ -26,7 +26,7 @@ public class GroupingTheMultiplePrgmsTest extends BaseClass {
 		//driver.findElement(By.xpath("//input[@name=\"firstname\"]")).sendKeys("Raj");
         driver.findElement(By.xpath("//input[@name=\"lastname\"]")).sendKeys("Kumar");
 		driver.findElement(By.xpath("(//img[@title=\"Select\"])[1]")).click();
-		System.out.println("Fine");
+		System.out.println("Good");
 		Thread.sleep(3000);
 		
 		
